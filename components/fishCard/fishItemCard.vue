b5fadaa7198d53db4cc3bf95234178e50b0a9387
Uğur Şölen <76258170+solen13@users.noreply.github.com>
2022-11-28 16:55:08 +0300

    28.11.22

--- components/fishCard/fishItemCard.vue
+++ components/fishCard/fishItemCard.vue
@@ -1,6 +1,6 @@
 <template>
    <v-main>
-      <v-card>
+      <v-card  >
           <div v-if="item['Species Illustration Photo']">
             <nuxt-img :src="item['Species Illustration Photo'].src"  width="300"  height="169" />
             <h1 class="text-center mt-2 " style="font-size: 16px">{{item['Species Name']}}</h1>
@@ -8,28 +8,35 @@
 
           </div>
       </v-card>
+
+
+
    </v-main>
 </template>
 
 <script>
 export default {
   name: "fishItemCard",
-  props:{
-    item:{
-      type:Object,
-      default:Object
+
+
+
+  props: {
+    item: {
+      type: Object,
+      default: Object
     }
   },
-  methods:{
-    btn(){
-     let items= this.item['Species Name']
-      this.$router.push('/detail/'+items)
+
+
+  methods: {
+    btn() {
+      let items = this.item['Species Name']
+      this.$router.push('/detail/' + items)
     },
 
-  }
+  },
+
 }
 </script>
 
-<style scoped>
 
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,40 @@
 <template>
 <v-main>
   <v-row>
-    <v-col  v-for="item in fishItem">
-      <fish-item-card   :item="item"/>
-    </v-col>
+
+      <v-col  v-for="item in fishItem">
+        <fish-item-card :item="item"/>
+      </v-col>
+
+
   </v-row>
+
+
 </v-main>
 </template>
 
 <script>
 import FishItemCard from "../components/fishCard/fishItemCard";
+import VueObserveVisibilityPlugin from 'vue-observe-visibility'
 export default {
   name: 'IndexPage',
   components: {FishItemCard},
+
   computed:{
     fishItem(){
       return  this.$store.state.fishItems
     },
+
+
   },
+
   created() {
     this.$store.dispatch('fishItem')
-  }
+  },
+
 }
 </script>
+
+<style scoped>
+
+</style>
