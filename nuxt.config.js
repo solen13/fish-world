b26beabc2e6ac422798fda1092d623255b03e95d
solen13 <uusolen@gmail.com>
2022-12-01 00:41:38 +0300

    01.12.22

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
