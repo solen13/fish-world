8e97e40865fcf94e4930d259ee12ac609d37502c
ugur-solen <76258170+solen13@users.noreply.github.com>
2022-12-12 00:48:18 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,11 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+     static:'target',
+     ssr: false,
+    generate: {
+    fallback: true
+    },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'fish',
